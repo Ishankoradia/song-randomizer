b74cef727b1f2ff45f47ab8d50bada3ef463776b
Ishankoradia <koradiaishan335@gmail.com>
2023-01-29 15:56:24 +0530

    setting routes in frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -6,6 +7,11 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/songs" element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
