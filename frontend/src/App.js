a19f632c7d4f027f556d8c2007d7e45ea14dd461
Ishankoradia <koradiaishan335@gmail.com>
2023-02-06 08:41:07 +0530

    fully responsive song randomizer, fixed the aysnc select initial issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
 // Components
 import Songs from "./components/Songs";
-import Songs1 from "./components/Songs1";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Songs />} />
           <Route path="/songs" element={<Songs />} />
-          <Route path="/songs1" element={<Songs1 />} />
         </Routes>
       </Router>
     </>
